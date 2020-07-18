e1f39a9397c78da1ac4921f6a041a80b0ab8a9ce
Imagineee <imagineeelego@gmail.com>
2020-07-18 14:12:07 +0100

    Add files via upload

---
+++ src/install.js
@@ -0,0 +1,28 @@
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+deferredInstallPrompt = evt;
+installButton.removeAttribute('hidden');
+
+deferredInstallPrompt.prompt();
+// Hide the install button, it can't be called twice.
+evt.srcElement.setAttribute('hidden', true);
+
+deferredInstallPrompt.userChoice
+    .then((choice) => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt', choice);
+      } else {
+        console.log('User dismissed the A2HS prompt', choice);
+      }
+      deferredInstallPrompt = null;
+    });
+
+    window.addEventListener('appinstalled', logAppInstalled);
+
+    console.log('Wood Note Installed!👏', evt);
+
+    @media all and (display-mode: standalone) {
+  body {
+    background-color: #b52b12;
+  }
+}
---
+++ src/service-worker.js
@@ -0,0 +1,74 @@
+const FILES_TO_CACHE = [
+  'index.html',
+];
+
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
+
+evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+);
+
+if (evt.request.mode !== 'navigate') {
+  // Not a page navigation, bail.
+  return;
+}
+evt.respondWith(
+    fetch(evt.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+              .then((cache) => {
+                return cache.match('offline.html');
+              });
+        })
+);
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+  '',
+  'index.html',
+  'app.js',
+  'FileSaver.js',
+  'manifest.json.js',
+  'style.css',
+];
+if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+if (evt.request.url.includes('/forecast/')) {
+  console.log('[Service Worker] Fetch (data)', evt.request.url);
+  evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            }).catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+      }));
+  return;
+}
+evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request)
+          .then((response) => {
+            return response || fetch(evt.request);
+          });
+    })
+);
