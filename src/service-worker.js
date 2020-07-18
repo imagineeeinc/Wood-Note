db821d0671a073a27fc1f5fe0d5969c09683d63e
Imagineee <imagineeelego@gmail.com>
2020-07-18 14:59:08 +0100

    Add files via upload

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,6 +2,10 @@ const FILES_TO_CACHE = [
   'index.html',
 ];
 
+self.addEventListener("activate", event => {
+  self.clients.claim();
+});
+
 evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
