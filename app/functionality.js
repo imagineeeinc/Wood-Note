58f55b92d3ab5c8a8b9e01dc1f3be5583860914c
Imagineee <imagineeelego@gmail.com>
2020-07-18 15:52:13 +0100

    Update functionality.js

--- app/functionality.js
+++ app/functionality.js
@@ -1,19 +1,3 @@
-  if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-  // Registration was successful
-  console.log('Registered!');
-  }, function(err) {
-  // registration failed :(
-  console.log('ServiceWorker registration failed: ', err);
-  }).catch(function(err) {
-  console.log(err);
-  });
-  });
-  } else {
-  console.log('service worker is not supported');
-}
-
 window.onload = function() {
     var menu = document.getElementById('fmenu');
     menu.className = 'shownmenu';
@@ -62,32 +46,8 @@ function save() {
 function clear() {
     document.getElementById('area').value = "";
 }
-
-//Dev Note Is a test note taking app made using web technoliges. this was developed in DevMoon
-
-
-/*function home() {
-	location.reload()
-}
-
 function about() {
-//	document.getElementById('about').onclick = function() {
-    document.getElementById('container').innerHTML = "";
-    //document.getElementById('menu').className = 'hiddenmenu';
-    var container = document.getElementById('container');
-    var p = document.createElement('p');
-    p.id = 'aboutus';
-
-    container.appendChild(p);
-    var text = document.createTextNode("Version Info[(Version:2.2.1) (Update: The File IO Update) (UpdateDescription: This Adds some UI Enhancements, a bullet point note feature, Give a name to the file you are saving and Load a file.)]");
-    p.appendChild(text);
-
-    container.appendChild(p);
-    var text = document.createTextNode("Technoliges[HTML, css, Javascript & FileSaver.js]");
-    p.appendChild(text);
-}*/
-function about() {
-    alert("Version:2.2.2\n\nUpdate: The File IO Update & Enhancments\n\nUpdateDescription: This Adds some UI Enhancements, a bullet point note feature, Give a name to the file you are saving and Load a file & performance enhnacments.");
+    alert("This is a special terrible edition for fun.");
 }
 //}
 function fmenuopen() {
@@ -131,137 +91,12 @@ function hmenuopen() {
         }*/
     }
 
-function lomenuopen() {
-        var menu = document.getElementById('lomenu');
-        if(menu.className != 'shownmenu') {
-            menu.className = 'shownmenu';
-        }
-        else {
-            menu.className = 'hhiddenmenu';
-        }
-    }
-
-function savefailas() {
-	var textFile = null,
-        makeTextFile = function(text) {
-            var data = new Blob([text], {
-                type: 'text/plain'
-            });
-
-            // If we are replacing a previously generated file we need to
-            // manually revoke the object URL to avoid memory leaks.
-            if (textFile !== null) {
-                window.URL.revokeObjectURL(textFile);
-            }
-
-            textFile = window.URL.createObjectURL(data);
-
-            return textFile;
-        }
-    var create = document.getElementById('create'),
-            textbox = document.getElementById('textbox');
-
-        create.addEventListener('click', function() {
-            var link = document.getElementById('downloadlink');
-            link.href = makeTextFile(textbox.value);
-            link.style.display = 'block';
-            document.getElementById('create').style.display = 'none';
-        }, false);
-    }
-
-function saveasother() {
-	var data = document.getElementsByName('textarea')[0].value;
-	window.open("data:application/text," + encodeURIComponent(data), "_self");
-}
-
-function saveas(textarea) {
-    var Name = "wood_note_page.txt"
-    var Name = prompt("Please enter your the name of the text file", "wood_note_page.txt");
-    if (Name == "") {
-        alert("you can not save in that name")
-    } else {
-        var data = document.getElementsByName('textarea')[0].value;
-        var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
-        saveAs(blob, Name);
-    }
-}
-/*function lodf() {
-	document.getElementById('area').value = "";
-	document.getElementById('inputfile') 
-              
-            var fr=new FileReader(); 
-            fr.onload=function(){
-            	document.getElementById('area') 
-                        .textContent=fr.result;
-            } 
-              
-        fr.readAsText(this.files[0]);
-}*/
-
-/*function loadf(file) {
-	var fileToLoad = inputfile;
-	var fileReader = new FileReader();
-	fileReader.onload = function(fileToLoad) {
-		content = e.target.result;
-		console.log("File loaded properly!");;
-};
-document.getElementById('area').textContent=fileReader.result;
-fileReader.readAsText(this.files[0]);
-}*/
 function bullet() {
     var text = document.getElementById('area').value;
     document.getElementById('area').value = text + "\u2022 ";
 }
 //\u2022 for bullet point
 
-/*function loadf() {
-    document.querySelector("#load").addEventListener('click', function() {
-    if(document.querySelector("#inputfile").files.length == 0) {
-        alert('Error : No file selected');
-        return;
-    var file = document.querySelector("#inputfile").files[0];
-    var reader = new FileReader();
-    reader.addEventListener('loadstart', function() {
-    console.log('File reading started');
-    reader.addEventListener('load', function(e) {
-    var text = e.target.result;
-
-    document.getElementById('area').value = text;
-    });
-
-    reader.addEventListener('error', function() {
-        alert('Error : Failed to read file');
-    });
-
-    reader.addEventListener('progress', function(e) {
-        if(e.lengthComputable == true) {
-            var percent_read = Math.floor((e.loaded/e.total)*100);
-            console.log(percent_read + '% read');
-        }
-    });
-
-    reader.readAsText(file);
-});*/
-
-function loadf(that) {
-        if(that.files && that.files[0]){
-            var reader = new FileReader();
-            reader.onload = function (e) {  
-                var output=e.target.result;
-                //window.confirm("Are You Sure You Want To Over Right The Text That Is In The Text Box");
-
-                if (confirm("Are You Sure You Want To Over Right The Text That Is In The Text Box")) {
-                    document.getElementById('area').value = output;
-                }
-                else {
-                alert("You Cancelled")
-            }
-            };
-            reader.readAsText(that.files[0]);
-        }
-}
-function signin() {
-}
 function copy() {
     var copyText = document.getElementById("area");
     /* Select the text field */
