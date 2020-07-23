75383976acdf68dc00fef771cea3cde909bc4f07
Imagineee <imagineeelego@gmail.com>
2020-07-23 18:46:37 +0100

    Add files via upload

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,10 +2,10 @@ const staticCacheName = 'site-static-v1';
 const assets = [
   '/',
   'index.html',
-  'app.js',
-  'FileSaver.js',
+  'src/app.js',
+  'src/FileSaver.js',
   'manifest.json.js',
-  'style.css',
+  'src/style.css',
 ];
 // install event
 self.addEventListener('install', evt => {
@@ -36,6 +36,45 @@ self.addEventListener('fetch', evt => {
   );
 });
 
+const dynamicCacheName = 'site-dynamic-v1';
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key =>  key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
+    })
+  );
+});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 /*const FILES_TO_CACHE = [
   '',
   'index.html',
