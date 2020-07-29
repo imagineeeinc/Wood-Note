57ed13b8c8997ab27079c58f150fbd90267f0877
Imagineee <imagineeelego@gmail.com>
2020-07-29 13:58:56 +0100

    Update pwabuilder-sw.js

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -16,7 +16,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.add(index.html))
   );
 });
 
