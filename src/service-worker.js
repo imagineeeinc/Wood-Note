3a8e0d36ed6497d17a52b0e82b2fe2a64940c045
Imagineee <imagineeelego@gmail.com>
2020-07-31 16:50:46 +0100

    Add files via upload

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,63 +1,32 @@
-if( 'function' === typeof importScripts) {
-  importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-  //addEventListener('message', onMessage);   
-}
-if (workbox) {
-  console.log(`Workbox is loaded :)`);
-} else {
-  console.log(`Workbox didn't load :(`);
-}
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+const cacheName = `wood note`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/src/style.css`,
+        `/src/app.js`,
+        `/src/emojiPicker.js`,
+        `/Favicon/favicon.ico`,
+        `/offline.html`,
+        `/manifest.json`
+      ])
+          .then(() => self.skipWaiting());
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-
-const FILES_TO_CACHE = [
-  '/',
-  'index.html',
-  'src/app.js',
-  'src/FileSaver.js',
-  'manifest.json.js',
-  'src/style.css',
-  'src/emojiPicker.js',
-];
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
